6961bfabaa753fac7cbc3bc08a41d7ededd296aa
Srujan Kada <srujan.kada@gmail.com>
2017-01-18 22:04:37 +0000

    added scss support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,77 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule }  from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MultistepBaseComponent } from './multistep-base/multistep-base.component';
+import { MultistepComponent } from './multistep/multistep.component';
+import { StepOneComponent } from './step-one/step-one.component';
+import { StepTwoComponent } from './step-two/step-two.component';
+import { StepThreeComponent } from './step-three/step-three.component';
+import { StepFourComponent } from './step-four/step-four.component';
+
+import {MultistepService} from './multistep.service';
+
+import { AlertModule } from 'ng2-bootstrap/alert';
+
+import { ModalModule } from 'ng2-bootstrap/modal';
+
+const multistepRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/multistep',
+    pathMatch: 'full',
+  },
+  {
+    path: 'multistep',
+    component: MultistepComponent,
+    children: [
+      {
+        path: '',
+        component: MultistepBaseComponent,
+      },
+      {
+        path: 'step1',
+        component: StepOneComponent
+      },
+      {
+        path: 'step2',
+        component: StepTwoComponent
+      },
+      {
+        path: 'step3',
+        component: StepThreeComponent
+      }
+      ,
+      {
+        path: 'step4',
+        component: StepFourComponent
+      }
+    ]
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultistepBaseComponent,
+    MultistepComponent,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent,
+    StepFourComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(multistepRoutes),
+    AlertModule.forRoot(),
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [MultistepService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
